d16d9133e5c4fe03771accfe265f1616a53045d1
kwubbenhorst <140316693+kwubbenhorst@users.noreply.github.com>
2024-03-05 23:20:12 -0500

    changes to Resume and AboutMe pages. Added text to bio blurb and layed out proficienciences on the Resume page. Added styles there.

--- src/components/UI/ProfileSection/ProfileSection.jsx
+++ src/components/UI/ProfileSection/ProfileSection.jsx
@@ -16,9 +16,15 @@ const ProfileSection = () => {
             My congregational leadership background has honed my skills in communication, collaboration, project management,
             and organizational systems. It was during my ministry years that I became interested in the power of well-designed
             tech media as a platform for The Message and as a first-line interface between organizations and their subscribers.
-            At a little beyond mid-career, I took on the challenge of learning to code, and while my learning will continue forever,
-            I look forward to launching myself in this new career and tailoring my growth to what is needed in my role at your company.
-            I enjoy travelling, writing, and gardening, and am happy to travel for work or work remotely.
+            <br></br><br></br>At a little beyond mid-career, I took on the challenge of learning to code. I have been coding intensively 
+            every day since July 2023 and have discovered a love for the building process. If only there were enough hours in the day to
+            develop every useful, elegant app my mind can envision! Both front and back-end work have their appeal for me. Thanks to a rigorous 
+            course of formal instruction (completed March 2024) I have developed clean coding habits and an awareness of best practices 
+            and current industry trends. At this point I am proficient in the MERN stack and associated technologies (see full CV and highlighted 
+            proficiencies at the resume page) and have the necessary foundation in programming logic that will allow me to expand my languages array beyond Javascript as a next step. While my learning 
+            will continue forever, I look forward to launching myself in this new career and tailoring my growth to what is needed in my role at 
+            your company. I can consider on-location employment in the Guelph / GTA / Kitchener-Waterloo area or in Calgary where I also have a 
+            home, though my preference is for working remotely. When I'm not coding, I enjoy travelling, writing, and gardening.
         </p>
       </div>
     </div>
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -4,53 +4,56 @@ import './Resume.css';
 
 const Resume = () => {
   return (
-    <div className='main-content resume'>
-      <h2>Resume</h2>
-      <div className='cv-download-link-container'>
-      <a className='cv-download-link' href={pdfPath} target="_blank" rel="noopener noreferrer">
-        Open as downloadable pdf
-      </a>
+    <div className='main-content'>
+      <div className='heading-and-link'>
+        <h2>Resume</h2>
+        <div className='cv-download-link-container'>
+          <a className='cv-download-link' href={pdfPath} target="_blank" rel="noopener noreferrer">
+            Open full CV as pdf for download
+          </a>
+        </div>
       </div>
 
-      <div className="resume-column">
-        <h3>Front-end Proficiencies</h3>
-        <ul>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>Javascript</li>
-          <li>JQuery</li>
-          <li>Responsive design</li>
-          <li>React</li>
-          <li>Bootstrap</li>
-        </ul>
-      </div>
+      <div className='proficiencies-row'>
+        <div className='resume-column'>
+          <h3>Front-end Proficiencies</h3>
+          <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>Javascript</li>
+            <li>JQuery</li>
+            <li>Responsive design</li>
+            <li>React</li>
+            <li>Bootstrap</li>
+          </ul>
+        </div>
 
-      <div className="resume-column">
-        <h3>Back-end Proficiencies</h3>
-        <ul>
-          <li>APIs</li>
-          <li>Node</li>
-          <li>Express</li>
-          <li>MySQL, Sequelize</li>
-          <li>Mongo DB, Mongoose</li>
-          <li>REST</li>
-          <li>GraphQL</li>
-        </ul>
-      </div>
+        <div className='resume-column'>
+          <h3>Back-end Proficiencies</h3>
+          <ul>
+            <li>APIs</li>
+            <li>Node</li>
+            <li>Express</li>
+            <li>MySQL, Sequelize</li>
+            <li>Mongo DB, Mongoose</li>
+            <li>REST</li>
+            <li>GraphQL</li>
+          </ul>
+        </div>
 
-      <div className="resume-column">
-        <h3>Soft-skills</h3>
-        <ul>
-          <li>Oral & Written Communication</li>
-          <li>High-level & Detail-oriented analysis</li>
-          <li>Collaboration & Independence</li>
-          <li>Leadership & People-skills</li>
-        </ul>
+        <div className='resume-column'>
+          <h3>Soft-skills</h3>
+          <ul>
+            <li>Excellence in Oral & Written Communication</li>
+            <li>High-level & Detail-oriented analytical ability</li>
+            <li>Able to work Collaboratively or Independently</li>
+            <li>Skills in Leadership & Working with People</li>
+            <li>High in Conscientiousness & Openness</li>
+            <li>An Agile-minded, Life-long Learner</li>
+            <li>Able to use AI & other tools judiciously</li>
+          </ul>
+        </div>
       </div>
-
-      <a href={pdfPath} target="_blank" rel="noopener noreferrer">
-        Open as downloadable pdf
-      </a>
     </div>
   );
 };
