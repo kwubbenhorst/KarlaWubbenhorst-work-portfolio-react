3a89265b1034e45880ff0f27d6291261cc409b1d
kwubbenhorst <140316693+kwubbenhorst@users.noreply.github.com>
2024-03-06 05:49:49 -0500

    amplified README.md, changes to Portfolio.jsx and Portfolio.css and ProjectCard.jsx, and ProjectCard.css to adapt the labels and props on the cards and, add icons and logic to conditionally render them. Technologies props needed to be prepared

--- src/components/UI/ProjectsSection/ProjectCard.jsx
+++ src/components/UI/ProjectsSection/ProjectCard.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import './ProjectCard.css';
+import { FaGithub, FaVideo } from 'react-icons/fa';
+import { IoMdGlobe } from 'react-icons/io';
 
-const ProjectCard = ({ title, deployLink, githubLink, image }) => {
+const ProjectCard = ({ title, deployLink, githubLink, image, hasWalkthroughVideo, technologies }) => {
   return (
     <div className="project-card">
       <img className="project-card-img" src={image} alt={title} />
       <div className="project-details">
-        <h1>
+        <h1 className="project-title">
           <a href={deployLink} target="_blank" rel="noopener noreferrer">
-            {title}
+            {title}{' '}
+            {hasWalkthroughVideo ? (<FaVideo title="Walkthrough Video" />) : (<IoMdGlobe title="Deployed Application" />)}
           </a>
         </h1>
-        <h2>
+        <h2 className="technologies-byline">
           <a href={githubLink} target="_blank" rel="noopener noreferrer">
-            Technologies
+            {technologies ? `${technologies} ` : ''}<FaGithub title="Repo at Github" />
           </a>
         </h2>
       </div>
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -10,16 +10,18 @@ const Portfolio = () => {
       <h2 className="fabulously-fs-title">Fabulously Full-Stack</h2>  
         <div className="project-row">
           <ProjectCard
-            title="Brimloft"
+            title="Brimloft: Ecommerce/Hat Store"
             deployLink="https://brim-loft-22a58ce3b819.herokuapp.com/"
             githubLink="https://github.com/kwubbenhorst/brim-loft"
             image="../../src/assets/images/brim-loft-screenshot.png"
+            technologies="HTML, CSS, JavaScript, Handlebars, Node.js, Express, MySQL, Sequelize, bcrypt, Session, Session Storage"
           />
           <ProjectCard
-            title="Offline Text Editor"
+            title="Offline Text Editor: PWA Application"
             deployLink="https://off-line-text-editor-2797b90d319f.herokuapp.com/"
             githubLink="https://github.com/kwubbenhorst/offline-text-editor"
             image="../../src/assets/images/offline-text-editor-screenshot.png"
+            technologies="HTML, CSS, JavaScript(ES6+), Node.js, Express, IndexedDB, Webpack, Babel, Workbox, ServiceWorker, concurrently"
           />
         </div>
         <div className="project-row"> 
@@ -27,13 +29,15 @@ const Portfolio = () => {
             title="Note Taker"
             deployLink="https://mighty-peak-97705-5192cd0d820f.herokuapp.com/"
             githubLink="https://github.com/kwubbenhorst/"
-            image="../../src/assets/images/note-taker-walkthrough-video.gif" 
+            image="../../src/assets/images/note-taker-screenshot.png"
+            technologies="HTML, CSS, JavaScript, Node.js, Express.js, REST, fs module, db.json" 
           />
           <ProjectCard
             title="My Professional Portfolio"
             deployLink="#"
             githubLink="https://github.com/kwubbenhorst/KarlaWubbenhorst-work-portfolio-react"
-            image="../../src/assets/images/placeholder-screenshot.png"
+            image="../../src/assets/images/professional-portfolio-screenshot.png"
+            technologies="HTML, CSS, JavaScript, Node.js, React, Vite, React DOM, React-router-DOM, React icons, Font Awesome, Google Fonts"
           />
         </div>
       </div>
@@ -47,12 +51,14 @@ const Portfolio = () => {
             deployLink="https://kwubbenhorst.github.io/coding-assessment-quiz/"
             githubLink="https://github.com/kwubbenhorst/coding-assessment-quiz"
             image="../../src/assets/images/coding-assessment-quiz-screenshot.png"
+            technologies="HTML, CSS, JavaScript, Local Storage, Bootstrap"
           />
           <ProjectCard
             title="Workday Scheduler"
             deployLink="https://kwubbenhorst.github.io/work-day-scheduler/"
             githubLink="https://github.com/kwubbenhorst/work-day-scheduler"
             image="../../src/assets/images/workday-scheduler-screenshot.png"
+            technologies="HTML, CSS, JavaScript, Bootstrap, jQuery, Font Awesome, Google Fonts, day.js"
           />
         </div>
         <div className="project-row"> 
@@ -60,13 +66,15 @@ const Portfolio = () => {
             title="Weather Dashboard"
             deployLink="https://kwubbenhorst.github.io/weather-dashboard/"
             githubLink="https://github.com/kwubbenhorst/weather-dashboard"
-            image="../../src/assets/images/weather-dashboard-screenshot.png" 
+            image="../../src/assets/images/weather-dashboard-screenshot.png"
+            technologies="HTML, CSS, Bootstrap, JavaScript, jQuery, Google Fonts, Open Weather Map API, Timezone DB API, day.js" 
           />
           <ProjectCard
             title="Kitchen Elevation"
             deployLink="https://kwubbenhorst.github.io/kitchen_elevation/"
             githubLink="https://github.com/kwubbenhorst/kitchen_elevation"
             image="../../src/assets/images/kitchen-elevation-screenshot.png"
+            technologies="HTML, CSS, JavaScript, Bootstrap, Google Fonts, Local Storage, Spoonacular API, CocktailDB API"
           />
         </div>
       </div>
@@ -75,16 +83,20 @@ const Portfolio = () => {
       <div className="bad-ass-back-ends-container">
         <h2 className="bad-ass-title">Bad-Ass Back-Ends</h2>
           <ProjectCard
-            title="Thought Share API"
+            title="Thought Share API: Social Netwk backend"
             deployLink="https://drive.google.com/file/d/14F-yLkn3o3_rlxD4BSp84KkrM-qJ2VvU/view?usp=drive_link"
             githubLink="https://github.com/kwubbenhorst/thought-share-API"
-            image="../../src/assets/images/thought-share-API-walkthrough-video.mp4"
+            image="../../src/assets/images/thought-share-api-screenshot.png"
+            technologies="Node.js, Express, REST, MongoDB, Mongoose, moment.js"
+            hasWalkthroughVideo={true}
           />
           <ProjectCard
             title="Clothes and Music EStore"
             deployLink="https://drive.google.com/file/d/1aumZkV58yrni1EIrGTqx4hHYNj6gBZ03/view?usp=drive_link"
             githubLink="https://github.com/kwubbenhorst/clothes-and-music-estore"
-            image="../../src/assets/images/placeholder-screenshot.png"
+            image="../../src/assets/images/clothes-and-music-estore-screenshot.png"
+            technologies="Node.js, Express, REST, MySQL, Sequelize"
+            hasWalkthroughVideo={true}
           /> 
       </div>
     
@@ -95,13 +107,17 @@ const Portfolio = () => {
             title="SVG Logo Maker"
             deployLink="https://drive.google.com/file/d/1sFQZ27DPwe7CNRPli-_B7LwKHaMQNfgW/view?usp=drive_link"
             githubLink="https://github.com/kwubbenhorst/SVG-logo-maker"
-            image="../../src/assets/images/placeholder-screenshot.png"
+            image="../../src/assets/images/svg-logo-maker-screenshot.png"
+            technologies="Node.js, Inquirer, Jest, ESM (ECMAScript Modules)"
+            hasWalkthroughVideo={true}
           />
           <ProjectCard
             title="README Generator"
             deployLink="https://drive.google.com/file/d/1ce1wGR04fKJbhYsFqcbmykHqSks9MPMr/view?usp=drive_link"
             githubLink="https://github.com/kwubbenhorst/README-generator"
-            image="../../src/assets/images/placeholder-screenshot.png"
+            image="../../src/assets/images/readme-generator-screenshot.png"
+            technologies="Node.js, Inquirer, fs module"
+            hasWalkthroughVideo={true}
           />
       </div>
     </div>
