05e27533e533adacac292b5f9b715c62ca178b0a
kwubbenhorst <140316693+kwubbenhorst@users.noreply.github.com>
2024-03-07 00:11:35 -0500

    removed comments from main.jsx file as that is what is at main.jsx:1 and the console identifies this as a problem with the deployed github page which is throwing an error

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,3 @@
-// This file serves as the entry point for rendering the App component. It uses ReactDOM.render 
-// to render the entire React application inside the HTML element with the ID 'root'. 
-// The App component, in turn, uses React Router (BrowserRouter, Route, Routes, Outlet) to manage 
-// navigation and render different page components based on the specified routes. 
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
